b258604a9d3c50de6d6411fb0da68c46870d2ce1
Tamiriam Fernandes <tamiriamfernandes@gmail.com>
2022-12-23 23:05:36 -0300

    configurando css

--- filmes/vite.config.ts
+++ filmes/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({}) => {
+  return defineConfig({
+    server: {
+      host: '0.0.0.0',
+      port: 80,
+    },
+    preview: {
+      port: 80
+    },
+    plugins: [react()],
+    build: {
+      chunkSizeWarningLimit: 1500,
+    }
+  });
+}
